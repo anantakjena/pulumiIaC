605e52d05b891a97348dd4eed71c5cd24c5aee07
Vignesh.N <vignesh.natraj@capgemini.com>
2020-02-27 11:55:09 +0000

    create serviceBus

--- MyStack.cs
+++ MyStack.cs
@@ -1,3 +1,4 @@
+using System;
 using Pulumi;
 using Azure = Pulumi.Azure;
 
@@ -6,6 +7,21 @@ class MyStack : Stack
     public MyStack()
     {
         var resourceGroup = new Azure.Core.ResourceGroup("testResourceGroup",
-                new Azure.Core.ResourceGroupArgs {Location = "ukwest"});
+                new Azure.Core.ResourceGroupArgs { Location = "ukwest" });
+
+        var serviceBusNamespace = new Azure.ServiceBus.Namespace("testServiceBusNamespace", new Azure.ServiceBus.NamespaceArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = "Basic"
+        });
+
+        var messageQueue = new Azure.ServiceBus.Queue("testMessageQueue", new Azure.ServiceBus.QueueArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            NamespaceName = serviceBusNamespace.Name,
+            MaxSizeInMegabytes = 1024,
+            EnablePartitioning = false,
+            DefaultMessageTtl = System.Xml.XmlConvert.ToString(TimeSpan.FromSeconds(30))
+        });
     }
 }
